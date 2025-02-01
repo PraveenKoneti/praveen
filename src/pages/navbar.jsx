b971abf26f548d2ec59c932f9d4df727f9fb2610
PraveenKoneti <konetipraveen7@gmail.com>
2025-02-01 23:15:10 +0530

    Added Media Query

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -8,8 +8,16 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-sm navbar-dark bg-black sticky-top">
             <div className="container">
                 <h1 className="navbar-brand fs-2"> Praveen Koneti</h1>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-                <span className="navbar-toggler-icon"></span>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#mynavbar"
+                    aria-controls="mynavbar"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    >
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="mynavbar">
                     <ul className="navbar-nav ms-auto">
@@ -22,32 +30,26 @@ const Navbar = () => {
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/experience" className="nav-link active me-2"> <i className="bi bi-person-workspace me-1"></i> Experience </Link>
+                            <Link to="/experience" className="nav-link active"> <i className="bi bi-person-workspace me-1"></i> Experience </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/projects" className="nav-link active me-2"> <i className="bi bi-briefcase me-1"></i> Projects </Link>
+                            <Link to="/projects" className="nav-link active "> <i className="bi bi-briefcase me-1"></i> Projects </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/skills" className="nav-link active me-2"> <i className="bi bi-gear me-1"></i> Skills </Link>
+                            <Link to="/skills" className="nav-link active"> <i className="bi bi-gear me-1"></i> Skills </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/education" className="nav-link active me-2"> <i className="bi bi-mortarboard me-1"></i> Education </Link>
+                            <Link to="/education" className="nav-link active"> <i className="bi bi-mortarboard me-1"></i> Education </Link>
                         </li>
                         
                         <li className="nav-item">
-                            <Link to="/contact" className="nav-link active me-2"> <i className="bi bi-envelope me-1"></i> Contact </Link>
+                            <Link to="/contact" className="nav-link active"> <i className="bi bi-envelope me-1"></i> Contact </Link>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-
-    
-
-
     )
-
-
 }
 
 
